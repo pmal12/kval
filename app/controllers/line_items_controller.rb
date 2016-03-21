719fe82a3476e41378533a09c829a9000f54b567
Peteris Maleiko <pmaleiko@gmail.com>
2016-03-21 13:35:53 +0200

    Added "delete" to line items and whole cart with ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,7 @@
 class LineItemsController < ApplicationController
   skip_before_action :authorize, only: [:create, :destroy]
   include CurrentCart
-  before_action :set_cart, only: [:create]
+  before_action :set_cart, only: [:create, :destroy]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
